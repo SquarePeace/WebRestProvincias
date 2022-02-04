167604736fc719c81f416ec0c67bae2ad2c87852
Allysson Jesus Moscoso <allysson-moscoso500@outlook.com>
2022-02-04 16:45:12 -0400

    first commit

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.demo;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+class User {
+
+  private Long id;
+  private String name;
+  private String role;
+
+  User() {}
+
+  User(String name, String role) {
+
+    this.name = name;
+    this.role = role;
+  }
+
+  @Id
+  @GeneratedValue(strategy=GenerationType.IDENTITY)
+  public Long getId() {
+    return this.id;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public String getRole() {
+    return this.role;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setRole(String role) {
+    this.role = role;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+
+    if (this == o)
+      return true;
+    if (!(o instanceof User))
+      return false;
+      User employee = (User) o;
+    return Objects.equals(this.id, employee.id) && Objects.equals(this.name, employee.name)
+        && Objects.equals(this.role, employee.role);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.id, this.name, this.role);
+  }
+
+  @Override
+  public String toString() {
+    return "Employee{" + "id=" + this.id + ", name='" + this.name + '\'' + ", role='" + this.role + '\'' + '}';
+  }
+}
